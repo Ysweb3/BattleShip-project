64e2d409a99f7ee484d638e939fec8c76c4501fa
Ysweb3 <yssh88972@gmail.com>
2026-01-24 00:41:10 +0530

    add player class

---
+++ src/modules/player.js
@@ -0,0 +1,21 @@
+export class RealPlayer {
+    constructor() {
+        this.gameboard = new Gameboard();
+        this.name = "Player";
+
+    }
+}
+export class AIPlayer {
+    constructor() {
+        this.gameboard = new Gameboard();
+        this.name = "AI";
+        
+    }
+}
+//TODO: add RealPlayer2 and setup option to switch between single player and two player
+// export class RealPlayer2{
+//     constructor() {
+//         this.gameboard = new Gameboard();
+//         this.name = "Player2";
+//     }
+// }
